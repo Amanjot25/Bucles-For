4645659040b1610b264152d6c8958f320409ff67
Amanjot25 <35691403+Amanjot25@users.noreply.github.com>
2018-02-15 13:43:09 +0100

    1

--- bucles.for1.py
+++ bucles.for1.py
@@ -3,23 +3,11 @@ print("PARES E IMPARES")
 num1 = int(input("Escribe un numero entero: "))
 num2 = int(input("Escribe un numero entero mayor o igual que (num1) : "))
 
-<<<<<<< HEAD
 if num2 < num1:
 	print ("¡Le he pedido un numero entero mayor que (num1):!")
-	     	
-=======
-if num2 > num1:
-	print ("¡Le he pedido un numero entero mayor que (num1):! ")
->>>>>>> b5149a9cce2f024956c05a679269f66b1163eea2
+
 else: 
 
     for i in range(1,num2+ 1):
         if i % 2 == 0:
-<<<<<<< HEAD
-	    print i
-  
-=======
-	    print("El numero (i) es par")
-        else:
-	    print("El numero (i) es impar")
->>>>>>> b5149a9cce2f024956c05a679269f66b1163eea2
+	   print i
